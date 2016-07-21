85f134d720f8d969a03b20d528d0253ccf6e9190
Franco <franco.liu@leediancn.com>
2016-07-22 00:54:18 +0800

    vdfvdfvdfvfdvfdv

---
+++ GitHubPractice/GitHubPractice/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitHubPractice
+//
+//  Created by franco on 2016/7/21.
+//  Copyright © 2016年 franco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitHubPractice/GitHubPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitHubPractice
+//
+//  Created by franco on 2016/7/21.
+//  Copyright © 2016年 franco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
